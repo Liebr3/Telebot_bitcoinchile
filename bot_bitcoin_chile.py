b6c6efe0b41268e0998b5d065283f205fea75987
liebr3 <liebredelmusgolibre@proton.me>
2025-04-15 11:42:42 -0400

    configurando start

--- bot_bitcoin_chile.py
+++ bot_bitcoin_chile.py
@@ -3,5 +3,14 @@ import telebot
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Hola, soy un bot de prueba. ¿Cómo puedo ayudarte?")
+def cmd_start(message):
+    """welcome to user"""
+    bot.reply_to(message, "Hola, como andamios!")
+    #bot.send_message(message.chat.id, "Hola, soy un bot de prueba. ¿Cómo puedo ayudarte?")
+
+#*************MAIN
+if __name__ == "__main__":
+    print("Start the bot")
+    bot.infinity_polling()
+    print("end")
+    
