661e4973b1bf6359f6d6691d064a217d56fd4f79
Liebr3 <Liebredelmusgolibre@gmail.com>
2025-04-15 17:29:22 -0400

    token agregado a .gitignore

--- bot_bitcoin_chile.py
+++ bot_bitcoin_chile.py
@@ -1,16 +1,23 @@
-from config import *
 import telebot
 
+from dotenv import load_dotenv
+import os
+
+# Cargar variables de entorno
+load_dotenv()
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
+
 @bot.message_handler(commands=['start'])
 def cmd_start(message):
     """welcome to user"""
     bot.reply_to(message, "Hola, como andamios!")
-    #bot.send_message(message.chat.id, "Hola, soy un bot de prueba. ¿Cómo puedo ayudarte?")
 
 #*************MAIN
 if __name__ == "__main__":
     print("Start the bot")
     bot.infinity_polling()
     print("end")
+#*************END
     
