26b0219b22ed4d52ee86f689969b3bf97a5f9427
Liebr3 <Liebredelmusgolibre@gmail.com>
2025-04-17 12:20:56 -0400

    Podando exceso de comentarios, ecribiendo menu de respuestas

--- bot_bitcoin_chile.py
+++ bot_bitcoin_chile.py
@@ -1,4 +1,5 @@
 import telebot
+import threading
 
 from dotenv import load_dotenv
 import os
@@ -6,15 +7,40 @@ import os
 # Cargar variables de entorno
 load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+print(f"TELEGRAM_TOKEN: '{TELEGRAM_TOKEN}'")  # Depuración
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
+
 
 @bot.message_handler(commands=['start'])
 def cmd_start(message):
     """welcome to user"""
     bot.reply_to(message, "Bienvenido al bot Bitcoin-Precio al instante " + "\n"
-                          "Cuando liebr3 y Copilot terminen les daré el precio "
-                          "de Bitcoin y otros valores en tiempo real" + "\n")            
+                          "✰ ℒiℯხřℯ_ďℯ_₿ọt 🄰🄻🄴🅁🅃 ✰" + "\n"
+                          "El bot esta en reparacion y en proceso de busqueda de servidor" + "\n")            
+
+# comandos del bot
+
+@bot.message_handler(commands=["btc", "eth", "dominance", "last ATH"])
+def price_command(message):
+    mensaje_text = message.text
+    if '/btc' in mensaje_text.lower(): #  in ['/btc', '/BTC', '/bitcoin']:
+        # url = "https://www.tradingview.com/symbols/BTCUSD/?exchange=BITSTAMP"
+        print("precio de bitcoin")
+        bot.send_message(message.chat.id, "Numbers goes up!" )
+    if '/dominance' in mensaje_text.lower(): #  in ['/btc', '/BTC', '/bitcoin']:
+        # url = "https://www.tradingview.com/symbols/BTCUSD/?exchange=BITSTAMP"
+        print("Dominancia de bitcoin")
+        bot.send_message(message.chat.id, "Muy alta, tristemente para los shitcoinlovers" )
+    if '/eth' in mensaje_text.lower(): #  in ['/btc', '/BTC', '/bitcoin']:
+        # url = "https://www.tradingview.com/symbols/BTCUSD/?exchange=BITSTAMP"
+        print("precio de ethereum")
+        bot.send_message(message.chat.id, "Numbers goes down!" )
+    if '/last ATH' in mensaje_text.lower(): #  in ['/btc', '/BTC', '/bitcoin']:
+        # url = "https://www.tradingview.com/symbols/BTCUSD/?exchange=BITSTAMP"
+        print("Ultimo ATH de bitcoin")
+        bot.send_message(message.chat.id, " $109.000 USD " )
+
 
 #*************MAIN
 if __name__ == "__main__":
